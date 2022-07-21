81f57709b76fd5d5b1453e33b0e5c2c684350823
itsjohncs <johnsullivan.pem@gmail.com>
2022-07-21 15:31:20 -0700

    Break out EU countries conveniently.

--- src/sum-vat-data.py
+++ src/sum-vat-data.py
@@ -4,6 +4,9 @@ import json
 import sys
 import functools
 import numbers
+import operator
+
+import pycountry
 
 EU_COUNTRY_CODES = {
     "AT",
@@ -36,32 +39,47 @@ EU_COUNTRY_CODES = {
 }
 
 
-def add_dicts(a, b):
+def add_dicts(a, b, op = operator.add):
     if isinstance(a, dict) and isinstance(b, dict):
         result = dict(a)
         for k, v in b.items():
             if k in result:
-                result[k] = add_dicts(result[k], v)
+                result[k] = add_dicts(result[k], v, op)
             else:
                 result[k] = v
 
         return result
     elif isinstance(a, numbers.Number) and isinstance(b, numbers.Number):
-        return a + b
+        return op(a, b)
     else:
         raise TypeError("a and b do not have compatible types")
 
 
-def main():
+def main(exchange_rate):
     data = json.load(sys.stdin)
+
     print(
         "EU: ",
         functools.reduce(
             add_dicts, (v for k, v in data.items() if k in EU_COUNTRY_CODES)
         ),
     )
+    for k, v in data.items():
+        if k not in EU_COUNTRY_CODES:
+            continue
+
+        # Net including all currencies
+        net_all = add_dicts(v["captured"], v["refunded"], operator.sub)
+        assert "usd" in net_all and len(net_all) == 1
+
+        net_euros = (net_all["usd"] / 100.0) / exchange_rate
+
+        print(f"\t{pycountry.countries.get(alpha_2=k).name}: {net_euros:.2f} EUR")
+
     print("UK: ", data["GB"])
 
 
 if __name__ == "__main__":
-    main()
+    exchange_rate = float(sys.argv[1])
+    print(f"Using 1 EUR = {exchange_rate} USD")
+    main(exchange_rate)
